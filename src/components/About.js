a5e1b9861b59a3d3bff6a9fb2eeea8f7e1f4b0f3
Jonathan Tan <jonathantan@Jonathans-MacBook-Air.local>
2023-11-26 15:07:18 +0800

    changed society name

--- src/components/About.js
+++ src/components/About.js
@@ -8,18 +8,18 @@ const About = () => {
                 <img src="https://images.squarespace-cdn.com/content/v1/5c222faff2e6b174633b3c87/1602271575891-SJSK411HS9S2BXR8NDIS/adopt-dog-cat-kitten-puppy-in-Atlanta-Midtown-local" alt="banner" class="bannerImg"/>
             </div>
             <div class="aboutCont">
-                <h1>Welcome to Pet Shelter!</h1>
+                <h1>Welcome to Pet Heaven!</h1>
 
-                <p>Where compassion meets companionship. At Pet Shelter, our mission is simple yet profound – to unite loving hearts
+                <p>Where compassion meets companionship. At Pet Heaven, our mission is simple yet profound – to unite loving hearts
                 with furry friends. As an advocate for animal welfare, we believe in providing a second chance for every paw,
                 creating forever homes filled with joy, laughter, and unconditional love.</p>
 
-                <p>Founded with a passion for making a positive impact on the lives of animals and people alike, Pet Shelter is not
+                <p>Founded with a passion for making a positive impact on the lives of animals and people alike, Pet Heaven is not
                 just a place to adopt a pet; it's a community that embraces the transformative power of the human-animal bond.
-                Whether you're here to find a loyal companion or looking to make a difference through volunteering, Pet Shelter
+                Whether you're here to find a loyal companion or looking to make a difference through volunteering, Pet Heaven
                 invites you to join us on this heartwarming journey.</p>
 
-                <p>Discover the joy of adoption, the warmth of a wagging tail, and the comfort of a purring friend. At Pet Shelter,
+                <p>Discover the joy of adoption, the warmth of a wagging tail, and the comfort of a purring friend. At Pet Heaven,
                 we're not just rehoming pets; we're building families and fostering lifelong connections.</p>
 
                 <h4>If you believe in our mission, sign up as a volunteer to be a member today!<br/><br/>Together, let's make a difference, one paw at a time.</h4>
